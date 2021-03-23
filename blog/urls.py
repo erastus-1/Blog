631a7873a8958e0d9c5e07d29b5d40664b07e45f
erastus <erastuskariuki15@gmail.com>
2021-03-23 22:19:31 +0300

    added models

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('registration/', RegistrationApiView.as_view(), name='registration'),
-    path('login/', LoginApiView.as_view(), name='login'),
+    path('api/registration/', RegistrationApiView.as_view(), name='registration'),
+    path('api/login/', LoginApiView.as_view(), name='login'),
 ]
