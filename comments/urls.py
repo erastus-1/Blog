784eba57acd61821bf0bc30ee153d2bcfc34c713
erastus <erastuskariuki15@gmail.com>
2021-04-01 11:27:45 +0300

    added comments pk

--- comments/urls.py
+++ comments/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('api/comments/comment/', CommentView.as_view(), name='comments'),
-    path('api/comments/comment/post/', CommentPostView.as_view(), name='article_post'),
+    path('api/comments/', CommentView.as_view(), name='comments'),
+    path('api/comments/post/', CommentPostView.as_view(), name='article_post'),
     
 ]
